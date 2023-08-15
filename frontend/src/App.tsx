0c7e4270a4846efb1f62652b776643b973dbcd17
nelsonf2019 <nelsonfercher@gmail.com>
2023-08-15 00:43:24 -0300

    First repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <div>
+      <header>
+        TS Amazona
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All rigth reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
