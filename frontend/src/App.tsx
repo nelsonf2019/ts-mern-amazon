bb8f90156fd8cb16fefc7be40a42291602bc8a4c
nelsonf2019 <nelsonfercher@gmail.com>
2023-08-15 03:00:40 -0300

    responsive site web

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,50 @@
 
 
-import './App.css'
+//import './App.css'
+import { Col, Container, Navbar, Nav, Row } from 'react-bootstrap'
 import { sampleProducts } from './Data'
 
 function App() {
    
 
-  return (
-    <div>
+  return (        //display flex
+    <div className='d-flex flex-column vh-100 '>
       <header>
-        TS Amazona
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>
+              ts amazone
+            </Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className='nav-link'>Cart</a>
+            <a href="/signin" className='nav-link'>Sign in</a>
+          </Nav>
+        </Navbar>
       </header>
-      <main>
-        <ul>
-        {
-          sampleProducts.map( product=>
-            <li key={product.slug}>
-              <img src={product.image} alt={product.name} 
-              className="product-image" 
-              />
-              <h2> {product.name} </h2>
-              <p> ${ product.price } </p>
-            </li>
-          )
-        }
-        </ul>
+       <main>               {/* margin top */}
+        <Container className='mt-3'>
+          <Row>
+          {
+            sampleProducts.map( product=>
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img src={product.image} alt={product.name} 
+                className="product-image" 
+                />
+                <h2> {product.name} </h2>
+                <p> ${ product.price } </p>
+              </Col>
+            )
+          }
+          </Row>
+        </Container>
+       
       </main>
       <footer>
+        <div className='text-center'>
         All rigth reserved
+        </div>
+        
       </footer>
     </div>
   )
